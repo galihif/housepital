95453a3d22cfe195ddcf086353f9535508f70218
galihif <galihindra2401@mail.ugm.ac.id>
2021-06-27 18:44:44 +0700

    modify search doc comp

--- src/components/SearchDoctors.jsx
+++ src/components/SearchDoctors.jsx
@@ -5,19 +5,18 @@ import React from 'react'
 import './SearchDoctors.scss'//Styles
 import { 
     Button,
-    Jumbotron,
     Row,
     Col,
     Form,
     FormControl
  } from 'react-bootstrap';
+import AppointmentCard from './AppointmentCard';
 
- //Images
 
 const SearchDoctors = () => {
     return (
         <div className="grid p-5 search-doctors-container">
-            <p className="text-center">Search Doctors</p>
+            <p className="text-center title">Search Doctors</p>
             <Row className="d-flex my-5 justify-content-center">
                 <Col lg={4}>
                     <Form className="d-flex">
@@ -31,8 +30,27 @@ const SearchDoctors = () => {
                     </Form>
                 </Col>
             </Row>
-            <Row>
-                
+            <Row className="px-5 d-flex justify-content-center">
+                <Col lg={3} className="d-flex justify-content-center mb-3">
+                    <AppointmentCard
+                        doctorName="Bruce Banner"
+                        type="Gamma Scientist"/>
+                </Col>
+                <Col lg={3} className="d-flex justify-content-center mb-3">
+                    <AppointmentCard
+                        doctorName="Natasha Romanoff"
+                        type="Special Spy"/>
+                </Col>
+                <Col lg={3} className="d-flex justify-content-center mb-3">
+                    <AppointmentCard
+                        doctorName="Dr. Stephen Strange"
+                        type="Neurosurgeon SpecialistSpecialistSpecialistSpecialist"/>
+                </Col>
+                <Col lg={3} className="d-flex justify-content-center mb-3">
+                    <AppointmentCard
+                        doctorName="Dr. Stephen Strange"
+                        type="Neurosurgeon SpecialistSpecialistSpecialistSpecialist"/>
+                </Col>
             </Row>
         </div>
     )
